d3216f607cf51e8606f34c81c4ff671b88958cec
JC <jeanclaude.lequigan.22@usjr.edu.ph>
2025-04-25 15:37:52 +0800

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { HomePage } from './home/home.page';
+import { HomePageModule } from './home/home.module';
+
+import { LocalNotifications } from '@capacitor/local-notifications';
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    HomePageModule
+  ],
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,98 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Platform } from '@ionic/angular';
+import { LocalNotifications } from '@capacitor/local-notifications';
+import { App as CapacitorApp } from '@capacitor/app';
 
 @Component({
   selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
-  standalone: false,
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
+  standalone:false,
 })
-export class HomePage {
+export class HomePage implements OnInit, OnDestroy {
+  currentTime: string = '';
+  interval: any;
 
-  constructor() {}
+  pomodoroDuration = 25 * 60;
+  breakDuration = 5 * 60;
 
+  remainingTime: number = 0;
+  countdownActive = false;
+  isBreak = false;
+
+  timerInterval: any;
+  backHandler: any;
+
+  constructor(private platform: Platform) {}
+
+  ngOnInit() {
+    this.updateCurrentTime();
+    this.interval = setInterval(() => this.updateCurrentTime(), 1000);
+
+    this.backHandler = this.platform.backButton.subscribeWithPriority(9999, () => {
+      CapacitorApp.exitApp();
+    });
+  }
+
+  ngOnDestroy() {
+    clearInterval(this.interval);
+    clearInterval(this.timerInterval);
+    this.backHandler.unsubscribe();
+  }
+
+  updateCurrentTime() {
+    const now = new Date();
+    this.currentTime = now.toLocaleTimeString();
+  }
+
+  startPomodoroCycle() {
+    this.remainingTime = this.pomodoroDuration;
+    this.isBreak = false;
+    this.countdownActive = true;
+    this.runTimer();
+  }
+
+  runTimer() {
+    this.timerInterval = setInterval(() => {
+      if (this.remainingTime > 0) {
+        this.remainingTime--;
+      } else {
+        clearInterval(this.timerInterval);
+        this.sendNotification(this.isBreak ? 'Break over! Time to focus again.' : 'Pomodoro done! Take a short break.');
+        this.isBreak = !this.isBreak;
+        this.remainingTime = this.isBreak ? this.breakDuration : this.pomodoroDuration;
+        this.runTimer();
+      }
+    }, 1000);
+  }
+
+  sendNotification(message: string) {
+    LocalNotifications.schedule({
+      notifications: [
+        {
+          title: 'PomodoroApp',
+          body: message,
+          id: new Date().getTime(),
+          sound: 'beep.aiff',
+          schedule: { at: new Date() },
+          smallIcon: 'ic_stat_icon_config_sample',
+        }
+      ]
+    });
+
+    // Optional vibration (for Android)
+    if (navigator.vibrate) {
+      navigator.vibrate(1000);
+    }
+  }
+
+  formatTime(seconds: number): string {
+    const m = Math.floor(seconds / 60);
+    const s = seconds % 60;
+    return `${this.pad(m)}:${this.pad(s)}`;
+  }
+
+  pad(n: number): string {
+    return n < 10 ? '0' + n : n.toString();
+  }
 }
